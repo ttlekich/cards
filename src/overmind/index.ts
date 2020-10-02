e0684ce3277f74dc5ff38f615dbf0040299e180e
Travis Lekich <ttlekich@gmail.com>
2020-10-01 21:18:03 -0500

    refactoring state management to overmind

---
+++ src/overmind/index.ts
@@ -0,0 +1,19 @@
+import { IConfig } from "overmind";
+import { createHook } from "overmind-react";
+import { state } from "./state";
+import * as actions from "./actions";
+
+export const config = {
+    state,
+    actions,
+};
+
+declare module "overmind" {
+    interface Config extends IConfig<typeof config> {}
+}
+
+export const useOvermind = createHook<typeof config>();
+export const useState = createStateHook<typeof config>();
+export const useActions = createActionsHook<typeof config>();
+export const useEffects = createEffectsHook<typeof config>();
+export const useReaction = createReactionHook<typeof config>();
